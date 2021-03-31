87b3fef90d5d771e9d5a4f398192cd291d5c3b23
neverchange95 <74570805+neverchange95@users.noreply.github.com>
2021-03-31 09:00:05 +0200

    comments

--- bfs.js
+++ bfs.js
@@ -1,12 +1,21 @@
+/**
+ * This is the javascript file, which draws the maze and search a path from the start to the target
+ * @author neverchange95
+ * @version 1.0
+ */
+
+// Getting the canvas from html
 var can2 = document.getElementById("canvas2");
 var ctx2 = can2.getContext("2d");
 
+// Define the x,y coordinates for start and target node and a running variable so that the user can restart the alg.
 var startNodeX2;
 var startNodeY2;
 var targetNodeX2;
 var targetNodeY2;
 var running2 = false;
 
+// A array which contains the maze 
 // s = start, e = end, 0 = wall, 1 = path
 var maze2 = [
     "1111111111111111111111111111111111", //0
@@ -45,9 +54,13 @@ var maze2 = [
     "0000000000s00000000000000000000100", //33
     "0000000000000000000000000000000000"] //34
 
+// Call both functions to draw the grid and after that draw the maze inside the grid.
 drawGrid2();
 drawMaze2();
 
+/**
+ * This function draws the grid into the canvas.
+ */
 function drawGrid2() {
     ctx2.strokeStyle = "black";
 
@@ -64,7 +77,10 @@ function drawGrid2() {
     }
 }
 
-async function drawMaze2() {
+/**
+ * This function draws the maze into the grid
+ */
+function drawMaze2() {
     ctx2.strokeStyle = "black";
 
     for(var i = 0.5; i < 34; i++) {
@@ -84,6 +100,10 @@ async function drawMaze2() {
     }
 }
 
+/**
+ * This function creates a array named visited. In this array will be true stored, if the node is visited.
+ * After that the bfs-function is called and is searching for a path.
+ */
 function bfsHelper() {
     if(!running2) {
         running2 = true;
@@ -98,25 +118,33 @@ function bfsHelper() {
             }
             visited2.push(lengthX2);
         }
-        bfsIter(visited2,startNodeX2,startNodeY2);
+        bfsIter(visited2,startNodeX2,startNodeY2); // Start searching
     }
 }
 
+/**
+ * This is the bfs-function which is searching a path to the target.
+ * @param {Array} visited 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @returns nothing
+ */
 async function bfsIter(visited2,x,y) {
-    var stack2 = []; 
+    var queue = []; // Create a queue, to push the successor nodes in it
     var node2 = [x,y];
-    stack2.push(node2);
-    while(stack2.length !== 0) {
-        node2 = stack2.shift();
-        var x = node2[0];
-        var y = node2[1];
+    queue.push(node2);
+    while(queue.length !== 0) {
+        node2 = queue.shift(); // Get the first pushed element from the queue
+        var x = node2[0]; // Get the x-coordinate
+        var y = node2[1]; // Get the y-coordinate
         
         var helpX = node2[0] - 0.5;
         var helpY = node2[1] - 0.5;
 
-        visited2[helpY][helpX] = true;
+        visited2[helpY][helpX] = true; // Set the actual node as visited
 
         if(x === targetNodeX2 && y === targetNodeY2) {
+            // The target is found!
             drawPath2(x,y);
             console.log("Ziel gefunden!");
             running2 = false;
@@ -124,70 +152,65 @@ async function bfsIter(visited2,x,y) {
         }
         
         drawPath2(x,y);
-        await sleep2(100);
+        await sleep2(100); // Sleep 100 millis
 
         if(helpY-1 >= 0 && maze2[helpY-1][helpX] !== '0' && !visited2[helpY-1][helpX]) {
+            // successor up
             var newNode = [x,y-1];
-            let n = stack2[stack2.length-1];
+            let n = queue[queue.length-1];
             if(n !== undefined) {
                 if(n[0] !== newNode[0] || n[1] !== newNode[1]) {
-                    stack2.push(newNode);
+                    queue.push(newNode);
                 }
             } else {
-                stack2.push(newNode);
+                queue.push(newNode);
             }
         } 
         if(helpX+1 < 34 && maze2[helpY][helpX+1] !== '0' && !visited2[helpY][helpX+1]) {
+            // successor down
             var newNode = [x+1,y];
-            let n = stack2[stack2.length-1];
+            let n = queue[queue.length-1];
             if(n !== undefined) {
                 if(n[0] !== newNode[0] || n[1] !== newNode[1]) {
-                    stack2.push(newNode);
+                    queue.push(newNode);
                 }
             } else {
-                stack2.push(newNode);
+                queue.push(newNode);
             }
         } 
         if(helpX-1 >= 0 && maze2[helpY][helpX-1] !== '0' && !visited2[helpY][helpX-1]) {
+            // successor left
             var newNode = [x-1,y];
-            let n = stack2[stack2.length-1];
+            let n = queue[queue.length-1];
             if(n !== undefined) {
                 if(n[0] !== newNode[0] || n[1] !== newNode[1]) {
-                    stack2.push(newNode);
+                    queue.push(newNode);
                 }
             } else {
-                stack2.push(newNode);
+                queue.push(newNode);
             }
         } 
         if(helpY+1 < 34 && maze2[helpY+1][helpX] !== '0' && !visited2[helpY+1][helpX]) {
+            // successor right
             var newNode = [x,y+1];
-            let n = stack2[stack2.length-1];
+            let n = queue[queue.length-1];
             if(n !== undefined) {
                 if(n[0] !== newNode[0] || n[1] !== newNode[1]) {
-                    stack2.push(newNode);
+                    queue.push(newNode);
                 }
             } else {
-                stack2.push(newNode);
+                queue.push(newNode);
             }
         }
     }
     return;
 }
 
-async function drawFoundPath2(path2) {
-    while(path2.length > 0) {
-        var coordinates2 = path2[0];
-        ctx2.beginPath();
-        ctx2.rect(coordinates2[0]*15,coordinates2[1]*15,15,15);
-        ctx2.fillStyle = "green";
-        ctx2.fill();
-        ctx2.stroke();
-        path2.shift();
-        await sleep2();
-    }
-    running2 = false;
-}
-
+/**
+ * This function draws the actual visited node black
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function drawPath2(x,y) {
     ctx2.beginPath();
     ctx2.rect(x*15,y*15,15,15);
@@ -196,7 +219,11 @@ function drawPath2(x,y) {
     ctx2.stroke();
 }
 
-
+/**
+ * This function draws the wall
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setWall2(x,y) {
     ctx2.beginPath();
     ctx2.rect(x*15,y*15,15,15);
@@ -205,6 +232,11 @@ function setWall2(x,y) {
     ctx2.stroke();
 }
 
+/**
+ * This function draws the start node green
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setStart2(x,y) {
     ctx2.beginPath();
     ctx2.rect(x*15,y*15,15,15);
@@ -213,6 +245,11 @@ function setStart2(x,y) {
     ctx2.stroke();
 }
 
+/**
+ * This function draws the target node blue
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setTarget2(x,y) {
     ctx2.beginPath();
     ctx2.rect(x*15,y*15,15,15);
@@ -221,14 +258,11 @@ function setTarget2(x,y) {
     ctx2.stroke();
 }
 
-function debugBacktracking(x,y) {
-    ctx2.beginPath();
-    ctx2.rect(x*15,y*15,15,15);
-    ctx2.fillStyle = "yellow";
-    ctx2.fill();
-    ctx2.stroke();
-}
-
+/**
+ * This function set the thread on sleep
+ * @param {Number} millis 
+ * @returns Promise 
+ */
 function sleep2(millis) {
     return new Promise(resolve => setTimeout(resolve,millis));
 }
--- dfs.js
+++ dfs.js
@@ -1,12 +1,21 @@
-var can = document.getElementById("canvas1");
+/**
+ * This is the javascript file, which draws the maze and search a path from the start to the target
+ * @author neverchange95
+ * @version 1.0
+ */
+
+// Getting the canvas from html
+var can = document.getElementById("canvas1"); 
 var ctx = can.getContext("2d");
 
+// Define the x,y coordinates for start and target node and a running variable so that the user can restart the alg.
 var startNodeX;
 var startNodeY;
 var targetNodeX;
 var targetNodeY;
 var running = false;
 
+// A array which contains the maze 
 // s = start, e = end, 0 = wall, 1 = path
 var maze = [
     "1111111111111111111111111111111111", //0
@@ -45,9 +54,13 @@ var maze = [
     "0000000000s00000000000000000000100", //33
     "0000000000000000000000000000000000"] //34
 
+// Call both functions to draw the grid and after that draw the maze inside the grid.
 drawGrid();
 drawMaze();
 
+/**
+ * This function draws the grid into the canvas.
+ */
 function drawGrid() {
     ctx.strokeStyle = "black";
 
@@ -64,7 +77,10 @@ function drawGrid() {
     }
 }
 
-async function drawMaze() {
+/**
+ * This function draws the maze into the grid
+ */
+function drawMaze() {
     ctx.strokeStyle = "black";
 
     for(var i = 0.5; i < 34; i++) {
@@ -84,6 +100,10 @@ async function drawMaze() {
     }
 }
 
+/**
+ * This function creates a array named visited. In this array will be true stored, if the node is visited.
+ * After that the dfs-function is called and is searching for a path.
+ */
 function dfsHelper() {
     if(!running) {
         running = true;
@@ -98,25 +118,33 @@ function dfsHelper() {
             }
             visited.push(lengthX);
         }
-        dfsIter(visited,startNodeX,startNodeY);
+        dfsIter(visited,startNodeX,startNodeY); // Start searching
     }
 }
 
+/**
+ * This is the dfs-function which is searching a path to the target.
+ * @param {Array} visited 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @returns nothing
+ */
 async function dfsIter(visited,x,y) {
-    var stack = []; 
-    var node = [x,y];
+    var stack = []; // Create a stack, to push the successor nodes in it
+    var node = [x,y]; 
     stack.push(node);
     while(stack.length !== 0) {
-        node = stack.pop();
-        var x = node[0];
-        var y = node[1];
+        node = stack.pop(); // Get the last pushed element from the stack
+        var x = node[0]; // Get the x-coordinate
+        var y = node[1]; // Get the y-coordinate
         
         var helpX = node[0] - 0.5;
         var helpY = node[1] - 0.5;
 
-        visited[helpY][helpX] = true;
+        visited[helpY][helpX] = true; // Set the actual node as visited
 
         if(x === targetNodeX && y === targetNodeY) {
+            // The target is found!
             drawPath(x,y);
             console.log("Ziel gefunden!");
             running = false;
@@ -124,22 +152,25 @@ async function dfsIter(visited,x,y) {
         }
         
         drawPath(x,y);
-        await sleep(100);
+        await sleep(100); // Sleep 100 millis
 
-        backtracking = true;
         if(helpY-1 >= 0 && maze[helpY-1][helpX] !== '0' && !visited[helpY-1][helpX]) {
+            // successor up
             var newNode = [x,y-1];
             stack.push(newNode);
         } 
         if(helpX+1 < 34 && maze[helpY][helpX+1] !== '0' && !visited[helpY][helpX+1]) {
+            // successor down
             var newNode = [x+1,y];
             stack.push(newNode);
         } 
         if(helpX-1 >= 0 && maze[helpY][helpX-1] !== '0' && !visited[helpY][helpX-1]) {
+            // successor left
             var newNode = [x-1,y];
             stack.push(newNode);
         } 
         if(helpY+1 < 34 && maze[helpY+1][helpX] !== '0' && !visited[helpY+1][helpX]) {
+            // successor right
             var newNode = [x,y+1];
             stack.push(newNode);
         }
@@ -147,7 +178,11 @@ async function dfsIter(visited,x,y) {
     return;
 }
 
-
+/**
+ * This function draws the actual visited node black
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function drawPath(x,y) {
     ctx.beginPath();
     ctx.rect(x*15,y*15,15,15);
@@ -156,6 +191,11 @@ function drawPath(x,y) {
     ctx.stroke();
 }
 
+/**
+ * This function draws the wall
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setWall(x,y) {
     ctx.beginPath();
     ctx.rect(x*15,y*15,15,15);
@@ -164,6 +204,11 @@ function setWall(x,y) {
     ctx.stroke();
 }
 
+/**
+ * This function draws the start node green
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setStart(x,y) {
     ctx.beginPath();
     ctx.rect(x*15,y*15,15,15);
@@ -172,6 +217,11 @@ function setStart(x,y) {
     ctx.stroke();
 }
 
+/**
+ * This function draws the target node blue
+ * @param {Number} x 
+ * @param {Number} y 
+ */
 function setTarget(x,y) {
     ctx.beginPath();
     ctx.rect(x*15,y*15,15,15);
@@ -180,6 +230,11 @@ function setTarget(x,y) {
     ctx.stroke();
 }
 
+/**
+ * This function set the thread on sleep
+ * @param {Number} millis 
+ * @returns Promise 
+ */
 function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve,millis));
 }
